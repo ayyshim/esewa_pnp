69ba9949fbb331d4e71bfdd4975360dd8b6c7644
Ashim Upadhaya <justayyme@gmail.com>
2022-03-06 15:49:39 +0545

    update readme for verison 2.0.0

--- lib/esewa_pnp.dart
+++ lib/esewa_pnp.dart
@@ -3,7 +3,6 @@
  * Author : Ashim Upadhaya
  */
 import 'dart:convert';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'esewa.dart';
